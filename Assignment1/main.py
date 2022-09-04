72100d71e7e303df74b1fe466a621f0071d3b364
Berlun Devera <berlun_devera@icloud.com>
2022-09-03 20:18:57 -0700

    updated test cases

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,4 +1,6 @@
+# Python code for a gumball vending machine
 class Machine:
+    # Initialize variables
     def __init__(self):
         self.balance = 0
         self.red = 0
@@ -7,34 +9,34 @@ class Machine:
         self.half_dollar = 0
         self.dollar_coin = 0
 
-
+    # Inserts a nickel in the machine and update the balance
     def insert_nickel(self):
         self.balance = self.balance + 5
         print('New balance: ' + str(self.balance))
     
-
+    # Inserts a dime in the machine and update the balance
     def insert_dime(self):
         self.balance = self.balance + 10
         print('New balance: ' + str(self.balance))
 
-
+    # Inserts a dime in the machine and update the balance
     def insert_quarter(self):
         self.balance = self.balance + 25
         print('New balance: ' + str(self.balance))
     
-
+    # Inserts a penny in the machine
     def insert_penny(self):
         self.pennies = self.pennies + 1
 
-
+     # Inserts a half dollar in the machine
     def insert_half_dollar(self):
         self.half_dollar = self.half_dollar + 1
 
-
+     # Inserts a dollar coin in the machine
     def insert_dollar_coin(self):
         self.dollar_coin = self.dollar_coin + 1
 
-
+    # Pull lever for red gumball which costs 5 cents
     def red_lever(self):
         if (self.pennies > 0) or (self.half_dollar > 0) or (self.dollar_coin > 0):
             print("Returned " + str(self.pennies) + " pennies, " + str(self.half_dollar) + " half dollars, and " + str(self.dollar_coin) + " dollar coins.")
@@ -50,7 +52,7 @@ class Machine:
             print("Number of red gumballs: " + str(self.red))
             print("Number of yellow gumballs: " + str(self.yellow))
 
-
+    # Pull lever for yellow gumball which costs 10 cents
     def yellow_lever(self):
         if (self.pennies > 0) or (self.half_dollar > 0) or (self.dollar_coin > 0):
             print("Returned " + str(self.pennies) + " pennies, " + str(self.half_dollar) + " half dollars, and " + str(self.dollar_coin) + " dollar coins.")
@@ -66,12 +68,12 @@ class Machine:
             print("Number of red gumballs: " + str(self.red))
             print("Number of yellow gumballs: " + str(self.yellow))
 
-    
+    # Returns change and resets the balance inside the gumball vending machine
     def return_my_change(self):
         print("Returned " + str(self.balance) + " cents")
         self.balance = 0
 
-
+# Driver code for gumball vending machine
 def main():
     machine = Machine()
     print("Valid actions:")
@@ -107,7 +109,8 @@ def main():
             machine.yellow_lever()
         elif action == "change":
             machine.return_my_change()
-        elif action == "done":
+        elif action == "done" and machine.balance > 0:
+            machine.return_my_change()
             x = 0
         else:
             print("Invalid action")
