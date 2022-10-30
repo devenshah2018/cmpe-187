d2b5bdf1fe8b3caa9dfa99907252bbe273ed0cea
Berlun Devera <berlun_devera@icloud.com>
2022-10-29 17:18:01 -0700

    changed output type from array to string

--- game_expo.py
+++ game_expo.py
@@ -1,44 +1,50 @@
+# An instance of a Child class contains the variables age and gender.
 class Child:
     def __init__(self, age, gender):
-        self.age = age
-        self.gender = gender
+        self.age = age  # The age of the child in integer form
+        self.gender = gender  # The gender of the child ("boy" or "girl")
 
 
+# A function that takes a Child class as its input parameters and returns a ticket containing the list of competitions that the child can participate in
 def issue_ticket(Child):
-    competitions = []
-    if (Child.gender == "boy" and Child.age > 7 and Child.age < 10):
-        competitions.append("Storytelling")
+    competitions = ""  # Initialize ticket list
+    if (Child.gender == "boy" and Child.age > 7 and Child.age < 10):  # First scenario
+        competitions = "Storytelling"
 
-    if (Child.gender == "girl" and Child.age > 7 and Child.age < 10):
-        competitions.append("Drawing")
+    if (Child.gender == "girl" and Child.age > 7 and Child.age < 10):  # Second scenario
+        competitions = "Drawing"
 
-    if (Child.gender == "boy" and Child.age > 11 and Child.age < 15):
-        competitions.append("Quiz")
+    if (Child.gender == "boy" and Child.age > 11 and Child.age < 15):  # Third scenario
+        competitions = "Quiz"
 
-    if (Child.gender == "girl" and Child.age > 10 and Child.age < 15):
-        competitions.append("Essay Writing")
+    if (Child.gender == "girl" and Child.age > 10 and Child.age < 15):  # Fourth scenario
+        competitions = "Essay Writing"
 
-    if ((Child.gender == "girl" or Child.gender == "boy") and Child.age < 6):
-        competitions.append("Rhyming")
+    if ((Child.gender == "girl" or Child.gender == "boy") and Child.age < 6):  # Fifth scenario
+        competitions = "Rhyming"
 
-    if ((Child.gender == "girl" or Child.gender == "boy") and Child.age > 20):
-        competitions.append("Poetry")
+    if ((Child.gender == "girl" or Child.gender == "boy") and Child.age > 20):  # Sixth scenario
+        competitions = "Poetry"
     print(competitions)
 
 
 def main():
-    student_1 = Child(8, "boy")
-    student_2 = Child(9, "girl")
-    student_3 = Child(13, "boy")
-    student_4 = Child(11, "girl")
-    student_5 = Child(5, "boy")
-    student_6 = Child(23, "boy")
-    issue_ticket(student_1)
-    issue_ticket(student_2)
-    issue_ticket(student_3)
-    issue_ticket(student_4)
-    issue_ticket(student_5)
-    issue_ticket(student_6)
+    child_1 = Child(9, "boy")
+    child_2 = Child(9, "girl")
+    child_3 = Child(12, "boy")
+    child_4 = Child(11, "girl")
+    child_5 = Child(5, "boy")
+    child_6 = Child(5, "girl")
+    child_7 = Child(21, "boy")
+    child_8 = Child(21, "girl")
+    issue_ticket(child_1)
+    issue_ticket(child_2)
+    issue_ticket(child_3)
+    issue_ticket(child_4)
+    issue_ticket(child_5)
+    issue_ticket(child_6)
+    issue_ticket(child_7)
+    issue_ticket(child_8)
 
 
 if __name__ == "__main__":
---
+++ next_date.py
@@ -0,0 +1,91 @@
+# TODO
+# Function that validates the format of the input date and raises an error if the input is not in the correct format
+def checkFormat(date):
+    import datetime
+    try:
+        datetime.datetime.strptime(date, '%m/%d/%Y')
+    except ValueError:
+        raise ValueError(
+            "Incorrect format. Please enter the date in MM/DD/YYYY")
+
+
+# Function that checks if a year is a leap year
+def isLeapYear(year):
+    leap = False
+    if (year % 400 == 0) and (year % 100 == 0):
+        leap = True
+    elif (year % 4 == 0) and (year % 100 != 0):
+        leap = True
+    else:
+        leap = False
+    return leap
+
+
+# Function that prints out the next date
+def nextDate(date):
+    print("Input date: " + date)
+
+    # Checks format of date
+    checkFormat(date)
+
+    # Initialize variables
+    month = int(date[0:2])
+    day = int(date[3:5])
+    year = int(date[6:10])
+
+    # Advances the day by one
+    day += 1
+
+    # Arrays of months that have 31 and 30 days, respectively
+    months_31_days = [1, 3, 5, 7, 8, 10]
+    months_30_days = [4, 6, 9, 11]
+
+    # Checks if it's the last day of a month that has 31 days
+    if (day == 32 and month in months_31_days):
+        month += 1
+        day = 1
+
+    # Checks if it's the last day of a month that has 30 days
+    if (day == 31 and month in months_30_days):
+        month += 1
+        day = 1
+
+    # Checks if it's the last day of February
+    if (month == 2 and day == 30 and isLeapYear(year)):  # Leap year
+        month += 1
+        day = 1
+    elif (month == 2 and day == 29 and not isLeapYear(year)):  # Not a leap year
+        month += 1
+        day = 1
+
+    # Check if date is the last date of the year
+    if (month == 12 and day == 32):
+        month = 1
+        day = 1
+        year += 1
+
+    # Formatting and printing out the date only if the year is between 1900 and 2099 inclusively
+    if (year <= 1900 or year >= 2099):
+        print("Year is out of range. Please enter a year that is inclusively between 1900 and 2099")
+        return
+    elif (len(str(month)) < 2 and len(str(day)) < 2):  # Add 0 before the month and day
+        print("Next date: 0" + str(month) +
+              "/0" + str(day) + "/" + str(year))
+    elif (len(str(month)) < 2):  # Add 0 before the month
+        print("Next date: 0" + str(month) +
+              "/" + str(day) + "/" + str(year))
+    elif (len(str(day)) < 2):  # Add 0 before the day
+        print("Next date: " + str(month) +
+              "/0" + str(day) + "/" + str(year))
+    else:
+        print("Next date: " + str(month) +
+              "/" + str(day) + "/" + str(year))
+
+
+def main():
+    date = input("Enter date (MM/DD/YYYY): ")
+    nextDate(date)
+
+
+if __name__ == "__main__":
+    main()
